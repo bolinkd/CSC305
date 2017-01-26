c4d16cf7d96bcff660b7945b91ed72c28d49ee5c
bolinkd <williambolink@gmail.com>
2017-01-26 09:19:37 -0800

    Clean up

--- RayTracer/camera.cpp
+++ RayTracer/camera.cpp
@@ -8,15 +8,32 @@ camera::camera()
     this->origin = vec3(0.0,0.0,0.0);
 }
 
-camera::camera(vec3 _origin, vec3 _lower_left_corner, vec3 _horizontal, vec3 _vertical){
-    this->origin = _origin;
-    this->lower_left_corner = _lower_left_corner;
-    this->horizontal = _horizontal;
-    this->vertical = _vertical;
+camera::camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect, float aperture, float focus_dist){
+    this->lens_radius = aperture / 2;
+    float theta = vfov * M_PI/180;
+    float half_height = tan(theta/2);
+    float half_width = aspect * half_height;
+    this->origin = lookfrom;
+    this->w = unit_vector(lookfrom - lookat);
+    this->u = unit_vector(cross(vup, this->w));
+    this->v = cross(this->w, this->u);
+    this->lower_left_corner = origin - half_width*focus_dist*u - half_height*focus_dist*v - focus_dist*w;
+    this->horizontal = 2*focus_dist*half_width*u;
+    this->vertical = 2*focus_dist*half_height*v;
 }
 
-ray camera::get_ray(float u, float v){
-    return ray(this->origin, this->lower_left_corner + u*this->horizontal+ v*this->vertical - this->origin);
+vec3 random_in_unit_disk(){
+    vec3 p;
+    do{
+        p = 2.0*vec3(drand48(),drand48(),0) - vec3(1,1,0);
+    }while(dot(p,p) >= 1.0);
+    return p;
+}
+
+ray camera::get_ray(float s, float t){
+    vec3 rd = lens_radius*random_in_unit_disk();
+    vec3 offset = this->u * rd.x + this->v * rd.y;
+    return ray(this->origin + offset, this->lower_left_corner + s*this->horizontal+ t*this->vertical - this->origin - offset);
 }
 
 
--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -7,6 +7,37 @@
 #include "dielectric.h"
 #include "metal.h"
 
+hitable *random_scene() {
+    int n = 500;
+    hitable **list = new hitable*[n+1];
+    list[0] =  new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = drand48();
+            vec3 center(a+0.9*drand48(),0.2,b+0.9*drand48());
+            if ((center-vec3(4,0.2,0)).length() > 0.9) {
+                if (choose_mat < 0.8) {  // diffuse
+                    list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+                }
+                else if (choose_mat < 0.95) { // metal
+                    list[i++] = new sphere(center, 0.2,
+                            new metal(vec3(0.5*(1 + drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())),  0.5*drand48()));
+                }
+                else {  // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
+
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+    return new hitable_list(list,i);
+}
+
 vec3 color(const ray& r, hitable *world, int depth) {
     hitrecord rec;
     if(world->hit(r, 0.001, FLT_MAX, rec)) {
@@ -30,11 +61,16 @@ int main()
     int ny = 100;
     int ns = 100;
 
-    //Set Up File
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    vec3 picture[ny][nx];
 
     //Set Up Objects
-    hitable *list[4];
+    hitable *list[5];
+
+    //float R = cos(M_PI/4);
+    //list[0] = new sphere(vec3(-R, 0, -1), R, new lambertian(vec3(0, 0, 1)));
+    //list[1] = new sphere(vec3(R, 0, -1), R, new lambertian(vec3(1, 0, 0)));
+
+
     list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.1,0.2,0.5)));
     list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8,0.8,0.0)));
     list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8,0.6,0.2), 1.0));
@@ -42,11 +78,25 @@ int main()
     list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5));
 
     //Set Up Camera
-    camera cam;
+    vec3 lookfrom(13,2,3);
+    vec3 lookat(0,0,0);
+    float dist_to_focus = 10.0;
+    float aperture = 0.1;
 
+    camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus);
+
+    /*
+    vec3 lookfrom = vec3(3, 3, 2);
+    vec3 lookat = vec3(0, 0, -1);
+    float dist_focus = (lookfrom - lookat).length();
+    //camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx)/float(ny), 2.0, dist_focus);
+    camera cam;
+*/
     hitable *world = new hitable_list(list, 5);
+    world = random_scene();
 
     //Iterate over pixels
+    #pragma omp parallel for
     for(int j= ny-1; j >= 0; j--){
         for(int i= 0; i<nx; i++ ){
             vec3 col(0,0,0);
@@ -57,7 +107,17 @@ int main()
                 col += color(r,world,0);
             }
             col /= float(ns);
-            col = vec3( sqrt(col.x), sqrt(col.y), sqrt(col.z));
+            picture[j][i] = vec3( sqrt(col.x), sqrt(col.y), sqrt(col.z));
+        }
+    }
+
+    //Set Up File
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+
+    // print colors
+    for(int j= ny-1; j >= 0; j--){
+        for(int i= 0; i<nx; i++ ){
+            vec3 col = picture[j][i];
             int ir = int(255.99*col.x);
             int ig = int(255.99*col.y);
             int ib = int(255.99*col.z);
